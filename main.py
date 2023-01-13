f3f6ede6ec4d7514c5ef62519ed150a7308bc31a
Shakeel Majeed <shakeelsmajeed@outlook.com>
2023-01-13 15:04:39 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #Sofos code
 from datetime import datetime,timedelta
+from test import *
 
 #first thing that will be run is this main function
 def main():
@@ -10,13 +11,14 @@ if __name__ == "__main__":
     start_time = datetime.now()
     now_time = datetime.now()
 
-    run_time = (3*60)-10 #-10 mins for now until we assess how long one round of our code will take because we need to stop our code before the 3 hours is up
+    run_time = (0.1) #-10 mins for now until we assess how long one round of our code will take because we need to stop our code before the 3 hours is up
+
+    i = 0
 
     #make program run for 2 hours 50 mins for now
     while (now_time < start_time + timedelta(minutes=run_time)):
-        main()
+        #main()
+        i = i+1
+        capture_image(i)
         # Update the current time
         now_time = datetime.now()
-    
-
-
