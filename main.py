7ef4b85920a06fc04601d63efa034ee39978663f
svy <67700169+svy473@users.noreply.github.com>
2023-02-09 14:56:23 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ from datetime import datetime,timedelta
 from test import *
 
 
+
+
+
 '''data.csv format:
     current coordinates
     nearest city 
     relative percentage area of live green vegetation in the image
 '''
-
+header = ['Percentage of live green vegetation in the image', 'Coordinate pair', 'Nearest City']
+with open('data.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
 #first thing that will be run is this main function
 def main():
     print("Hello Astro-Pi")
@@ -18,7 +24,7 @@ if __name__ == "__main__":
     start_time = datetime.now()
     now_time = datetime.now()
 
-    run_time = (0.1) #-10 mins for now until we assess how long one round of our code will take because we need to stop our code before the 3 hours is up
+    run_time = (0.5) #-10 mins for now until we assess how long one round of our code will take because we need to stop our code before the 3 hours is up
 
     i = 0
 
@@ -27,6 +33,15 @@ if __name__ == "__main__":
         #main()
         i = i+1
         capture_image(i)
+        directory = "/home/astropi/Documents/Sofos/images/image%s.jpg" % i
+
+        image = cv2.imread(directory)
+        contrasted = contrast_stretch(image)
+        ndvi = calc_ndvi(contrasted)
+        ndvi_contrasted = contrast_stretch(ndvi) #in greyscale which is hard for us to decipher
+        convert_colourscale(ndvi_contrasted, i)
+
         coordinates()
+
         # Update the current time
         now_time = datetime.now()
